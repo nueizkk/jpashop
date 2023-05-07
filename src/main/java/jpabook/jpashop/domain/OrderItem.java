a526e25aed5143438b7bbc8cf00f8abf7ccf584d
kimjieun <nueizkk@gmail.com>
2023-05-07 18:47:54 +0900

    feat: XToOne 지연로딩으로 변경

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -12,10 +12,10 @@ public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
-    @ManyToOne // 단방향이면 mappedBy 그냥 안쓰면 되는지?
+    @ManyToOne(fetch = FetchType.LAZY) // 단방향이면 mappedBy 그냥 안쓰면 되는지?
     @JoinColumn(name = "item_id")
     private Item item;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="order_id")
     private Order order;
     private int orderPrice;
