cd3b1edf63eb6e5b6495e7371075266a30c6484a
nueizkk <fejieun@luxurynall.com>
2023-05-09 14:09:18 +0900

    feat: 주문, 주문상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -21,4 +21,28 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStockQuantity(count);
+        return orderItem;
+    }
+
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStockQuantity(count);
+    }
+    
+    //==조회 로직==//
+    /**
+     * 주문상품 전체 가격 조회
+     * */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
